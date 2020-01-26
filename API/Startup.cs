8e3ed5e422cff9219858f906a440dee35007d48c
Michael Lucci <mdlucci@gmail.com>
2020-01-26 18:55:06 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-            
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -44,7 +50,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
