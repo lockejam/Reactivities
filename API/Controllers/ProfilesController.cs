c5f6eb4caffa868e91d4181d2346b6c181476f71
Michael Lucci <mdlucci@gmail.com>
2023-04-24 13:04:48 -0400

    End of section 21

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -16,9 +16,15 @@ namespace API.Controllers
         }
 
         [HttpPut]
-        public async Task<IActionResult> Edit(Edit.Command command) 
+        public async Task<IActionResult> Edit(Edit.Command command)
         {
             return HandleResult(await Mediator.Send(command));
+        }
+
+        [HttpGet("{username}/activities")]
+        public async Task<IActionResult> GetUserActivities(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new ListActivities.Query { Username = username, Predicate = predicate }));
         }
     }
 }
